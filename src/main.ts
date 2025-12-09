fb4f3ca9f53a43bd96b2e706a807fb94cd270da5
kimgeeumju <29376@naver.com>
2025-12-09 21:19:20 +0900

    Enable CORS for GitHub Pages frontend

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.enableCors({
     origin: [
       "http://localhost:3000",
